7730c51a6fd4196844d2f6f81dae7e34aac41ab9
davi1709 <davi.mmr10@gmail.com>
2026-09-04 00:14:10 -0300

    adicionei apllylists padrão
    Added a new cache for playlists and updated the current artist ID.

--- js/state.js
+++ js/state.js
@@ -1,4 +1,9 @@
+//AAAAAAAA
+let tipoMenuAtual = 'artistas'; // 'artistas' ou 'playlists'
+
 let artistaIdAtual = "6043160";
+let playlistIdAtual = "3155776842"; // Exemplo padrão
+
 let bancoDeMusicas = [];
 let albunsDisponiveis = [];
 let filaDeMusicas = [];
@@ -27,6 +32,9 @@ let pontosBonusClassico = 500;
 let historicoGeral = [];
 
 let catalogosCache = {}; 
+let catalogosPlaylistsCache = {};
+
+let artistaSelecionadoNome = "Kali Uchis";
 
 let listaArtistasPadrão = [
     { id: "6043160", nome: "Kali Uchis" },
@@ -39,4 +47,10 @@ let listaArtistasPadrão = [
     { id: "630", nome: "Björk" },
     { id: "6478881", nome: "SOPHIE" },
     { id: "14456487", nome: "Juice WRLD" }
+];
+
+let listaPlaylistsPadrao = [
+    { id: "3155776842", titulo: "Top Worldwide", capa: "https://cdn-images.dzcdn.net/images/playlist/0b92c2f9e94a1a3650f0de769c4fc07c/500x500-000000-80-0-0.jpg", fas: 50000 },
+    { id: "5207214368", titulo: "Top 50 Sertanejo", capa: "https://cdn-images.dzcdn.net/images/playlist/a0bfc66bf660f268503241e9ae95d045/500x500-000000-80-0-0.jpg", fas: 30000 },
+    { id: "8282573142", titulo: "10s Pop", capa: "https://cdn-images.dzcdn.net/images/playlist/a2c4a523d4054a2ddb04856f0f31282b/500x500-000000-80-0-0.jpg", fas: 40000 }
 ];
